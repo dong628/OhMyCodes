202587352286d78852f1c84b13e4bbc793708c3e
dong628 <1816750728@qq.com>
2020-05-01 17:15:24 +0800

    POJ1995-1

---
+++ POJ1995.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int qpow(int a, int b, int m){
+	int base=a, ans=1;
+	while(b){
+		if(b&1){
+			ans *= base;
+			ans %= m;
+		}
+		base *= base;
+		base %= m;
+		b >>= 1;
+	}
+	return ans;
+}
+
+int main(){
+	int z, m, a, b, h, sum;
+	scanf("%d", &z);
+	for(int x=0; x<z; x++){
+		sum = 0;
+		scanf("%d", &m); scanf("%d", &h);
+		for(int i=0; i<h; i++){
+			scanf("%d %d", &a, &b);
+			sum += qpow(a, b, m);
+			sum %= m;
+		}
+		printf("%d", sum);
+	}
+
+	return 0;
+}
