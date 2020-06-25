4cb32b9b85a26a403fef5e8bf74ffcaa80209a1e
dong628 <1816750728@qq.com>
2020-06-25 09:35:42 +0800

    POJ3889-1

---
+++ POJ3889.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#define ull unsigned long long
+using namespace std;
+
+int t, n;
+ull l, h, o;
+struct Point{
+	ull x;
+	ull y;
+}p, p1;
+
+Point calc(int, ull);
+
+int main(){
+	freopen("data.in", "r", stdin);
+	scanf("%d", &t);
+	for(int x=0; x<t; x++){
+		scanf("%d %llu %llu", &n, &h, &o);
+		Point a=calc(n, h-1);
+		Point b=calc(n, o-1);
+		l = (ull)(sqrt(double((a.x-b.x)*(a.x-b.x)*100+(a.y-b.y)*(a.y-b.y)*100))+0.5);
+		printf("%llu\n", l);
+	}
+
+	return 0;
+}
+
+Point calc(int level, ull nu){
+	if(level==1){
+		switch(nu){
+			case 0 : p.x=0; p.y=0; break; 
+			case 1 : p.x=0; p.y=1; break; 
+			case 2 : p.x=1; p.y=1; break; 
+			case 3 : p.x=1; p.y=0; break; 
+		};
+		return p;
+	}
+	else{
+		ull pre=(ull)(pow(4,level-1));
+		p=calc(level-1, nu%pre);
+		if(nu/pre == 0) { p1.x=p.y; p1.y=p.x; }
+		if(nu/pre == 1) { p1.x=p.x; p1.y=p.y+pow(2,level-1); }
+		if(nu/pre == 2) { p1.x=p.x+pow(2,level-1); p1.y=p.y+pow(2,level-1); }
+		if(nu/pre == 3) { p1.x=pow(2,level)-p.y-1; p1.y=pow(2,level-1)-p.x-1; }
+		return p1;
+	}
+}
