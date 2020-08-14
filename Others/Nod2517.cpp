e6a848fa89c0ca9ab7605b30a902d6f91ca0a010
dong628 <1816750728@qq.com>
2020-08-14 11:03:43 +0800

    51Nod2517

--- Others/Nod2517.cpp
+++ Others/Nod2517.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 const int Max=20005;
-int a[Max], n, zero, one, last;
+int a[Max], n, zero, one, last, qz[Max], hz[Max], minn=0x7fffffff;
 char x;
 
 int main(){
@@ -11,15 +11,19 @@ int main(){
 		scanf("%c", &x);
 		a[i]=x-'0';
 	}
-	int i;
-	for(i=n-1; i>=0; i--){
-		if(a[i]==0) break;
+	
+	qz[1]=a[0]; hz[n-1]=!a[n-1];
+	for(int i=1; i<n; i++){
+		qz[i+1]=qz[i]+a[i];
+		if(a[n-i-1]==0)
+			hz[n-i-1]=hz[n-i]+1;
+		else
+			hz[n-i-1]=hz[n-i];
 	}
-	for(; i>=0; i--){
-		if(a[i]==0) zero++;
-		else { one++; last=i; }
+	for(int i=0; i<=n; i++){
+		minn=minn<qz[i]+hz[i+1]?minn:qz[i]+hz[i+1];
 	}
-	printf("%d", std::min(zero-last, one));
+	printf("%d", minn);
 	
 	return 0;
 }
