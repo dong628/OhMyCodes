0e77fcf5ef9b524c6c2a2a82c907ff74998a7a46
dong628 <1816750728@qq.com>
2020-08-14 11:13:07 +0800

    51Nod1174

---
+++ Others/Nod1174.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <iostream>
+
+void init(void);
+
+int main(){
+	scanf("%d %d", &n, &m);
+	for(int i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	init();
+	
+	return 0;
+}
+
+void init(void){
+	
+}
