eaf3233aaff4b7a80d54d83d9f54177fa306da43
dong628 <1816750728@qq.com>
2020-08-15 11:23:25 +0800

    Nod2653

--- 51Nod/Nod2653.cpp
+++ 51Nod/Nod2653.cpp
@@ -7,13 +7,18 @@ int ans, a, b, bi, len;
 int main(){
 	scanf("%d %d", &a, &b);
 	bi=log(a)/log(2);
-	if(bi*bi<a) bi++;
-	len=log(b-bi)/log(2);
-	for(int i=a; i<bi*bi; i++){
-		ans = ans^i;
+	if((1<<bi)<a) bi++;
+	len=log(b-(1<<bi)+1)/log(2);
+	if(len==1 || (1<<bi)>=b) {
+		for(int i=a; i<=b; i++) ans = ans^i;
 	}
-	for(int i=bi*bi+len*len; i<=b; i++){
-		ans = ans^i;
+	else{
+		for(int i=a; i<(1<<bi); i++){
+			ans = ans^i;
+		}
+		for(int i=(1<<bi)+(1<<len); i<=b; i++){
+			ans = ans^i;
+		}
 	}
 	printf("%d", ans);
 	
