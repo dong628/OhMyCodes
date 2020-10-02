3fd7249824b253751831f073dfdc163cf26239e3
dong628 <1816750728@qq.com>
2020-10-02 10:39:58 +0800

    ZOJ3203 in Vjudge

---
+++ Others/ZOJ3203.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+
+int t;
+const double Esp=1e-10;
+double h1, h2, d, r, l;
+
+int main(){
+	scanf("%d", &t);
+	for(int tt=0; tt<t; tt++){
+		scanf("%lf %lf %lf", &h1, &h2, &d);
+		while(r-l<Esp){
+			mid=(l+r)/2;
+			mmid=(mid)+r/2;
+		}
+	}
+
+	return 0;
+}
