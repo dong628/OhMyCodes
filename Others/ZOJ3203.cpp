beb47973388b79e42d0f0578cb175e26f6f9dba8
dong628 <1816750728@qq.com>
2020-10-03 23:18:06 +0800

    ZOJ3203

--- Others/ZOJ3203.cpp
+++ Others/ZOJ3203.cpp
@@ -3,16 +3,23 @@
 
 int t;
 const double Esp=1e-10;
-double h1, h2, d, r, l;
+double h1, h2, d, r, l, mid, mmid;
+inline double calc(double x) { return h1-((h1-h2)/x*d)+(d-x); }
 
 int main(){
+	freopen("data.in", "r", stdin);
+
 	scanf("%d", &t);
 	for(int tt=0; tt<t; tt++){
 		scanf("%lf %lf %lf", &h1, &h2, &d);
-		while(r-l<Esp){
+		l=0; r=d;
+		while(r-l>Esp){
 			mid=(l+r)/2;
-			mmid=(mid)+r/2;
+			mmid=(mid+r)/2;
+			if(calc(mid)>calc(mmid)) r=mmid;
+			else l=mid;
 		}
+		printf("%.3lf ", calc(l));
 	}
 
 	return 0;
