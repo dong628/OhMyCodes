3f919eac85ecadf07a894a344d015c7949ecf276
dong628 <1816750728@qq.com>
2021-08-26 11:26:31 +0800

    8.26

--- Luogu/P3387.cpp
+++ Luogu/P3387.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 const int Maxn=1e2+5, Maxm=1e5+5;
 struct Edge{
@@ -8,12 +9,15 @@ struct Edge{
 } edge[Maxm];
 int n, m, u, v, tim, scnt, top;
 int a[Maxn], cnt[Maxn], cnts[Maxn], scc[Maxn][Maxn], stack[Maxn], dfn[Maxn], low[Maxn];
-int newcnt[Maxn], mst[Maxn], summ[Maxn];
+int newcnt[Maxn], mst[Maxn], summ[Maxn], rdrcnt[Maxn], rdrcnt2[Maxn], dp[Maxn], tp[Maxn], maxdp, tail;
 bool vis[Maxn];
-std::vector<int> mapp[Maxn], newmap[Maxn];
+std::vector<int> mapp[Maxn], newmap[Maxn], rdr[Maxn];
 
 void tarjan(int);
+//void topo(int);
+void topo(void);
 int min(int xx, int yy) { return xx<yy?xx:yy; }
+int max(int xx, int yy) { return xx>yy?xx:yy; }
 
 int main(){
 	freopen("data.in", "r", stdin);
@@ -51,6 +55,9 @@ int main(){
 	for(int i=1; i<=m; i++){
 		if(mst[edge[i].u]!=mst[edge[i].v]){
 			newmap[mst[edge[i].u]].push_back(mst[edge[i].v]);
+			rdr[mst[edge[i].v]].push_back(mst[edge[i].u]);
+			rdrcnt[mst[edge[i].v]]++;
+			rdrcnt2[mst[edge[i].v]]++;
 			newcnt[mst[edge[i].u]]++;
 		}
 	}
@@ -67,6 +74,28 @@ int main(){
 		printf("\n");
 	}
 */
+	topo();
+/*
+	for(int i=0; i<scnt; i++){
+		if(rdrcnt[i] == 0){
+			tp[tail++] = i;
+			vis[i] = true;
+			memset(vis, 0, sizeof(vis));
+			topo(i);
+			break;
+		}
+	}
+*/
+	dp[tp[0]] = summ[tp[0]];
+	for(int i=1; i<scnt; i++){
+		for(int j=0; j<rdrcnt2[tp[i]]; j++){
+			dp[tp[i]] = max(dp[tp[i]], dp[rdr[tp[i]][j]]+summ[tp[i]]);
+		}
+	}
+	for(int i=0; i<scnt; i++){
+		maxdp = max(maxdp, dp[i]);
+	}
+	printf("%d", maxdp);
 
 	return 0;
 }
@@ -97,5 +126,34 @@ void tarjan(int point){
 			top--;
 		}
 		scnt++;
+	}
+}
+/*
+void topo(int point){
+	for(int i=0; i<newcnt[point]; i++){
+		if(!vis[newmap[point][i]]){
+			tp[tail++] = newmap[point][i];
+			vis[newmap[point][i]] = true;
+			topo(newmap[point][i]);
+		}
+	}
+}
+*/
+void topo(void){
+	int point;
+	memset(stack, 0, sizeof(stack));
+	top = 0;
+	for(int i=0; i<scnt; i++){
+		if(rdrcnt[i] == 0) stack[top++] = i;
+	}
+	while(top>0){
+		point = stack[--top];
+		tp[tail++] = point;
+		for(int i=0; i<newcnt[point]; i++){
+			rdrcnt[newmap[point][i]]--;
+			if(rdrcnt[newmap[point][i]]==0){
+				stack[top++] = newmap[point][i];
+			}
+		}
 	}
 }
