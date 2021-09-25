09daab09097fec72dcf592bd4ec9a25e594ae816
dong628 <1816750728@qq.com>
2021-09-25 11:41:10 +0800

    925

---
+++ Luogu/P2023.cpp
@@ -0,0 +1,122 @@
+#include <cstdio>
+#include <iostream>
+
+typedef long long lol;
+const int Maxn=1e5+5;
+struct Node{
+	int l, r;
+	lol val, tagm, taga;
+} seg[4*Maxn];
+lol max(lol xx, lol yy) { return xx>yy?xx:yy; }
+lol min(lol xx, lol yy) { return xx<yy?xx:yy; }
+void build(int, int, int), update(int), mul(int, int, int, lol), add(int, int, int, lol);
+lol sig(int, int, int);
+int n, t, g, tp, m;
+lol Mod, c, a[Maxn];
+
+int main(){
+	freopen("data.in", "r", stdin);
+
+	scanf("%d %lld", &n, &Mod);
+	for(int i=0; i<n; i++) scanf("%lld", &a[i]);
+	build(0, 0, n-1);
+	scanf("%d", &m);
+	for(int csm=0; csm<m; csm++){
+		scanf("%d", &tp);
+		if(tp==1){
+			scanf("%d %d %lld", &t, &g, &c);
+			mul(0, t, g, c);
+		}
+		else if(tp==2){
+			scanf("%d %d %lld", &t, &g, &c);
+			add(0, t, g, c);
+		}
+		else{
+			scanf("%d %d", &t, &g);
+			printf("%lld\n", sig(0, t, g));
+		}
+	}
+
+	return 0;
+}
+
+void build(int root, int l, int r){
+	seg[root].l=l; seg[root].r=r;
+	seg[root].tagm=1;
+	if(l==r){
+		seg[root].val=a[l]%Mod;
+		return;
+	}
+	build(root<<1, l, (l+r)>>1);
+	build(root<<1+1, (l+r)>>1+1, r);
+	seg[root].val = seg[root<<1].val+seg[root<<1+1].val;
+}
+
+void add(int root, int l, int r, lol addv){
+	if(seg[root].l==l && seg[root].r==r){
+		seg[root].val += (seg[root].r-seg[root].l+1)*addv%Mod;
+		seg[root].taga += addv%Mod;
+		return;
+	}
+	update(root);
+	seg[root].val = 0;
+	if(seg[root<<1].r>=l){
+		add(root<<1, l, min(seg[root<<1].r, r), addv);
+	}
+	seg[root].val += seg[root<<1].val;
+	if(seg[root<<1+1].l<=r){
+		add(root<<1+1, max(seg[root<<1+1].l, l), r, addv);
+	}
+	seg[root].val += seg[root<<1+1].val;
+}
+
+void mul(int root, int l, int r, lol mulv){
+	if(seg[root].l==l && seg[root].r==r){
+		seg[root].val *= mulv%Mod;
+		seg[root].val %= Mod;
+		seg[root].taga *= mulv%Mod;
+		seg[root].taga %= Mod;
+		seg[root].tagm *= mulv%Mod;
+		seg[root].tagm %= Mod;
+		return;
+	}
+	update(root);
+	seg[root].val = 0;
+	if(seg[root<<1].r>=l){
+		mul(root<<1, l, min(seg[root<<1].r, r), mulv);
+	}
+	seg[root].val += seg[root<<1].val;
+	if(seg[root<<1+1].l<=r){
+		mul(root<<1+1, max(seg[root<<1+1].l, l), r, mulv);
+	}
+	seg[root].val += seg[root<<1+1].val;
+}
+
+lol sig(int root, int l, int r){
+	if(seg[root].l==l && seg[root].r==r){
+		return seg[root].val;
+	}
+	update(root);
+	lol ans=0;
+	if(seg[root<<1].r>=l){
+		ans += sig(root<<1, l, min(seg[root<<1].r, r))%Mod;
+	}
+	if(seg[root<<1+1].l<=r){
+		ans += sig(root<<1+1, max(seg[root<<1+1].l, l), r)%Mod;
+	}
+	ans%=Mod;
+	return ans;
+}
+
+void update(int root){
+	seg[root<<1].val *= seg[root].tagm; seg[root<<1].val %= Mod;
+	seg[root<<1].val += seg[root].taga*(seg[root<<1].r-seg[root<<1].l+1); seg[root<<1].val %= Mod;
+	seg[root<<1+1].val *= seg[root].tagm; seg[root<<1+1].val %= Mod;
+	seg[root<<1+1].val += seg[root].taga*(seg[root<<1+1].r-seg[root<<1+1].l+1); seg[root<<1+1].val %= Mod;
+	seg[root<<1].taga *= seg[root].tagm; seg[root<<1].taga%=Mod;
+	seg[root<<1].taga += seg[root].taga; seg[root<<1].taga%=Mod;
+	seg[root<<1].tagm *= seg[root].tagm; seg[root<<1].tagm%=Mod;
+	seg[root<<1+1].taga *= seg[root].tagm; seg[root<<1+1].taga%=Mod;
+	seg[root<<1+1].taga += seg[root].taga; seg[root<<1+1].taga%=Mod;
+	seg[root<<1+1].tagm *= seg[root].tagm; seg[root<<1+1].tagm%=Mod;
+}
