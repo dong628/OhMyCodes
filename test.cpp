4a4bd67ba7698226c00625a0d80b7f3a30213910
dong628 <1816750728@qq.com>
2023-03-05 01:50:25 +0000

    Sun Mar  5 01:50:25 AM UTC 2023

--- test.cpp
+++ test.cpp
@@ -1,53 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-#include <tuple>
-#include <cstring>
-
-const unsigned maxn = 1e5 + 10;
-
-const int inf = 0x3f3f3f3f;
-const int ninf = 0xc0c0c0c0;
-
-int dp_max[maxn], dp_min[maxn];
-
-std::vector<std::tuple<unsigned, int>> G[maxn];
-
-std::tuple<int, int> dp(unsigned u) {
-    if (dp_max[u] != ninf) {
-        assert(dp_min[u] != inf);
-        return {dp_max[u], dp_min[u]};
-    }
-    if(G[u].size() == 0) {
-        dp_max[u] = dp_min[u] = 0;
-        return {0, 0};
-    };
-    for(auto [v, w] : G[u]) {
-        auto [maxv, minv] = dp(v);
-        dp_max[u] = std::max(dp_max[u], maxv + w);
-        dp_min[u] = std::min(dp_min[u], minv + w);
-    }
-    return {dp_max[u], dp_min[u]};
+#include<bits/stdc++.h>
+using namespace std;
+long long n,k;
+bool b[1000000005];
+void er(){
+	for(long long i=2;i<=sqrt(n);i++){
+		long long y=i;
+		for(long long j=1;j<k;j++){
+			y*=i;
+		}
+		if(y<=n) b[y]=1;
+		else continue;
+//		cout<<i<<" "<<y<<endl;
+		for(long long j=1;j<=n;j++){
+			y*=i;
+			if(y<=n) b[y]=1;
+			else break;
+		}
+	}
+	return ;
 }
-
-int main() {
-freopen("data.in", "r", stdin);
-
-    unsigned n, m;
-    std::cin >> n >> m;
-
-    memset(dp_max, 0xc0, sizeof(decltype(dp_max[0])) * (n + 1));
-    memset(dp_min, 0x3f, sizeof(decltype(dp_min[0])) * (n + 1));
-
-    for(unsigned i = 0;i < m;i++) {
-        unsigned u, v;
-        int w;
-        std::cin >> u >> v;
-        std::cin >> w;
-
-        G[u].push_back({v, w});
-    }
-
-    auto [maxv, minv] = dp(1);
-    std::cout << maxv << " " << minv << '\n';
+int main()
+{
+//	freopen("power.in","r",stdin);
+//	freopen("power.out","w",stdout);
+	cin>>n>>k;
+	if(k==1){
+		cout<<n;
+		return 0;
+	} 
+	er();
+	long long sum=1;
+	for(long long i=2;i<=n;i++){
+		if(b[i]==1) {
+			sum++;
+//			cout<<i<<" ";
+		}
+	}
+//	cout<<endl;
+	cout<<sum;
+	return 0;
 }
