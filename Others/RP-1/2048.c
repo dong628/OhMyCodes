de5e916d3451b4a79cc62843b46525e8e65a4c36
dong628 <1816750728@qq.com>
2024-10-07 02:55:45 +0000

    Mon Oct  7 02:55:45 AM UTC 2024

--- Others/RP-1/2048.c
+++ Others/RP-1/2048.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
 int mat[4][4];
 void placenum(void), printmat(void);
+int rand24();
+bool gravity(char);
 
 int main(){
 	time_t t;
-	srand(unsigned(time(&t)));
+	srand((unsigned)(time(&t)));
 	char opt;
 	for( ; ; ){
 		placenum();
-		printmat();
 		while(true){
+			printmat();
 			printf("input option: ");
 			opt = getchar();
+			getchar();
 			if(gravity(opt)) break;
 		}
 	}
@@ -48,43 +52,80 @@ void printmat(void){
 }
 
 bool gravity(char opt){
-	int pmat[4][4];
-	if(opt == "W"){
+	int pmat[4][4], tmpmat[4][4];
+	if(opt == 's'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				pmat[i][j] = mat[3-i][j];
 			}
 		}
 	}
-	if(opt == "A"){
+	else if(opt == 'd'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				pmat[i][j] = mat[3-i][j];
+				tmpmat[i][j] = mat[i][3-j];
 			}
 		}
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				pmat[j][i] = mat[i][j];
+				pmat[j][i] = tmpmat[i][j];
 			}
 		}
 	}
-	if(opt == "S"){
+	else if(opt == 'w'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				pmat[i][j] = mat[i][j];
 			}
 		}
 	}
-	if(opt == "D"){
+	else if(opt == 'a'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				pmat[j][i] = mat[i][j];
 			}
 		}
 	}
+	else{
+		return false;
+	}
 
-	int drop_mat[4][4];
+/*
+	printf("turned:\n");
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			printf("|%d\t", pmat[i][j]);
+		}
+		printf("|\n");
+	}
+	printf("\n");
+*/
+
+
+	int drop_mat[10][10], merge_mat[10][10], layer, layer_last;
 	memset(drop_mat, 0, sizeof(drop_mat));
+	memset(merge_mat, 0, sizeof(merge_mat));
+	for(int j=0; j<4; j++){
+		layer = 0;
+		for(int i=0; i<4; i++){
+			if(pmat[i][j] != 0){
+				drop_mat[layer++][j] = pmat[i][j];
+			}
+		}
+		for(int i=0; i<4; i++){
+			if(drop_mat[i][j] == drop_mat[i+1][j]){
+				drop_mat[i][j] <<= 1;
+				drop_mat[i+1][j] = 0;
+			}
+		}
+		layer = 0;
+		for(int i=0; i<4; i++){
+			if(drop_mat[i][j] != 0){
+				merge_mat[layer++][j] = drop_mat[i][j];
+			}
+		}
+	}
+/*
 	for(int j=1; j<4; j++){
 		layer = 3;
 		for(int i=3; i>=0; i--){
@@ -98,48 +139,58 @@ bool gravity(char opt){
 			}
 		}
 	}
+*/
 	bool ret = false;
 	for(int i=0; i<4; i++){
 		for(int j=0; j<4; j++){
-			if(drop_mat[i][j] != pmat[i][j]){
+			if(merge_mat[i][j] != pmat[i][j]){
 				ret = true;
 			}
 		}
 	}
 
-	if(opt == "W"){
+	if(opt == 's'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				mat[i][j] = drop_mat[3-i][j];
+				mat[i][j] = merge_mat[3-i][j];
 			}
 		}
 	}
-	if(opt == "A"){
+	if(opt == 'd'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				mat[j][i] = drop_mat[i][j];
+				tmpmat[j][i] = merge_mat[i][j];
 			}
 		}
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				mat[i][j] = drop_mat[3-i][j];
-			}
-		}
-	}
-	if(opt == "S"){
-		for(int i=0; i<4; i++){
-			for(int j=0; j<4; j++){
-				mat[i][j] = drop_mat[i][j];
+				mat[i][j] = tmpmat[i][3-j];
 			}
 		}
 	}
-	if(opt == "D"){
+	if(opt == 'w'){
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
-				mat[j][i] = drop_mat[i][j];
+				mat[i][j] = merge_mat[i][j];
+			}
+		}
+	}
+	if(opt == 'a'){
+		for(int i=0; i<4; i++){
+			for(int j=0; j<4; j++){
+				mat[j][i] = merge_mat[i][j];
 			}
 		}
 	}
 
 	return ret;
+}
+
+int rand24(){
+	if(rand()%10 == 0){
+		return 4;
+	}
+	else{
+		return 2;
+	}
 }
